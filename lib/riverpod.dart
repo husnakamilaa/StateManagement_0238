ee117f6ced5c5362867123fb8701d1ebdfb1264d
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-04-04 02:56:09 +0700

    feat: membangun UI form

--- lib/riverpod.dart
+++ lib/riverpod.dart
@@ -27,3 +27,46 @@ class MyApp extends StatelessWidget {
 }
 
 //UI
+class FormPage extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final nama = ref.watch(namaProvider);
+    final email = ref.watch(emailProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Form Riverpod")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: "Nama"),
+              onChanged: (value) =>
+                  ref.read(namaProvider.notifier).state = value,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: "Email"),
+              onChanged: (value) =>
+                  ref.read(emailProvider.notifier).state = value,
+            ),
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  //SnackBar(content: Text("$nama - $email")),
+                  SnackBar(content: Text("Submit sukses !")),
+                );
+              },
+              child: Text("Submit"),
+            ),
+            SizedBox(height: 10),
+
+            Text("Nama: $nama"),
+            Text("Email: $email"),
+          ],
+        ),
+      ),
+    );
+  }
+}
