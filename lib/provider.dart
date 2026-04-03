efdddafbaa89097475211f37c71af9c47f429c2b
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-04-04 02:25:37 +0700

    feat: deklarasi context dari state provider

---
+++ lib/provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => FormProvider(),
+      child: MyApp(),
+    ),
+  );
+}
+
